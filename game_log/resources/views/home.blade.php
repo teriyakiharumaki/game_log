a91d2c805152628cbc6c1207105c89849a73a9b7
徳永直哉 <ieyasu1118@gmail.com>
2026-01-31 02:45:42 +0900

    トップページの仮作成

---
+++ game_log/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Game Log</title>
+  </head>
+  <body>
+    <h1>Game Log</h1>
+    <p>ゲームのレビューを記録するアプリ</p>
+  </body>
+</html>
