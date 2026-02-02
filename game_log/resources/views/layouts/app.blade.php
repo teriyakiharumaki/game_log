fe9d7112030a64f5eed8b31ea75480e7c3b70609
徳永直哉 <ieyasu1118@gmail.com>
2026-02-03 04:55:42 +0900

    トップページ、ヘッダーのタイトルをリンク化

--- game_log/resources/views/layouts/app.blade.php
+++ game_log/resources/views/layouts/app.blade.php
@@ -1,21 +1,20 @@
 <!doctype html>
 <html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title', 'Game Log')</title>
-</head>
-<body>
-  <header>
-    <h1>🎮 Game Log</h1>
-    <nav>
-      <a href="/">トップ</a>
-    </nav>
-    <hr>
-  </header>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Game Log')</title>
+  </head>
+  <body>
+    <header>
+      <h1>
+        <a href="/">🎮 Game Log</a>
+      </h1>
+      <hr>
+    </header>
 
-  <main>
-    @yield('content')
-  </main>
-</body>
+    <main>
+      @yield('content')
+    </main>
+  </body>
 </html>
