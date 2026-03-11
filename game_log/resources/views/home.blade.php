873f0d8880ca19e4d3cdf6c91605d8acb4ef9535
徳永直哉 <ieyasu1118@gmail.com>
2026-03-11 15:35:57 +0900

    ホーム画面のUIデザインの変更

--- game_log/resources/views/home.blade.php
+++ game_log/resources/views/home.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'トップページ')
 
 @section('content')
-  <p>ゲームのレビューを記録するアプリです。</p>
-
-  <a href="{{ route('games.index') }}">
-    ゲーム一覧
-  </a>
+  <div class="hero min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">GAME LOG</h1>
+        <p class="py-6">
+          ゲームのレビューを記録するアプリです。
+        </p>
+        <a href="{{ route('games.index') }}" class="btn btn-primary">
+          ゲーム一覧
+        </a>
+      </div>
+    </div>
+  </div>
 @endsection
